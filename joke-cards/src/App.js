0905bd749b676516eb2903c2401acaf6d12ed035
Tami Sweitzer <tamara0715@gmail.com>
2019-07-20 15:52:41 -0500

    Initial commit. React SPA App

---
+++ joke-cards/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Joke from './Joke';
+import { Jumbotron, Container, Row, Col} from 'reactstrap';
+import './App.css';
+
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      jokes: [
+        {
+          setup: "What time did the man go to the dentist?",
+          punchline: "Tooth hurt-y!"
+        },
+        {
+          setup: "Did you hear about the guy who invented Lifesavers?",
+          punchline: "They say me made a mint!"
+        },
+        {
+          setup: "Why do chicken coops only have two doors?",
+          punchline: "Because if they had four, they would be chicken sedans!"
+        },
+        {
+          setup: "Why did the Clydesdale give the pony a glass of water?",
+          punchline: "Because he was a little horse!"
+        }
+      ]
+    }
+  }
+
+ 
+  render() {
+    let jokeCards = this.state.jokes.map(humor => {
+      return (
+        <Col className="p-3" sm="4" key={humor.toString()} >
+          <Joke humor={humor} />
+        </Col>
+      )
+    })
+    
+    
+    return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">Joke Cards</h1>
+          <p className="lead">Welcome to the next billion dollar app taking the world by storm</p>
+          <hr className="my-2"/>
+          <p>Prepare to be amused!</p>
+          <p className="lead"></p>
+        </Jumbotron>
+        <Container fluid>
+          <Row>
+            { jokeCards }
+          </Row>
+        </Container>
+     </div>
+    )
+  }
+
+}
+
+
+
+
